0849e1bf64f75d5ea8f0f420a5a2b5185ebbab60
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2024-02-12 13:35:41 +0200

    initial mutation and query in convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
-import { SignInButton, SignOutButton, auth } from "@clerk/nextjs";
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Home() {
-  const { userId } = auth();
+  const { isSignedIn } = useSession();
+
+  const createThumbnail = useMutation(api.thumbnails.createThumbnail);
+
+  const thumbnails = useQuery(api.thumbnails.getThumbnailsForUser);
+
   return (
-    <main className="">{userId ? <SignOutButton /> : <SignInButton />}</main>
+    <main className="">
+      {isSignedIn ? <SignOutButton /> : <SignInButton />}
+
+      {isSignedIn && (
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const form = e.target as HTMLFormElement;
+            const formData = new FormData(e.currentTarget as HTMLFormElement);
+            const title = formData.get("title") as string;
+
+            await createThumbnail({ title });
+
+            form.reset();
+          }}
+        >
+          <label>Title</label>
+          <input name="title" className="text-black" />
+          <button>Create</button>
+        </form>
+      )}
+      {isSignedIn &&
+        thumbnails?.map((item) => <div key={item._id}>{item.title}</div>)}
+    </main>
   );
 }
